8e9d828903fcf6f6211e7eafaa23a84e97d30cac
caiotayota <caiotayota@live.com>
2022-05-08 04:10:47 +0100

    Register

--- js/script.js/script.js
+++ js/script.js/script.js
@@ -1,14 +1,3 @@
-function verifyPassword() {
-
-    var theForm = document.forms["login-form"];
-    var password = theForm.elements["password"].value;
-
-    if (password.length >= 8) {
-        $("#login-button").removeAttr('disabled');
-    } else {
-        $("#login-button").prop("disabled", true);
-    }
-}
 // When the user clicks on the password field, show the message box
 
 $("#password").focus(function () {
@@ -29,21 +18,75 @@ $("#password").keyup(function () {
     if ($("#password").val().length < 8) {
         $("#length").removeClass("valid");
         $("#length").addClass("invalid");
+        $("#login-button").prop("disabled", true);
     } else {
         $("#length").removeClass("invalid");
         $("#length").addClass("valid");
+        $("#login-button").removeAttr('disabled');
     }
 });
 
+$("#password").keyup(function () {
+    if ($("#password").val().length < 8) {
+        $("#length").removeClass("valid");
+        $("#length").addClass("invalid");
+        $("#login-button").prop("disabled", true);
+    } else {
+        $("#length").removeClass("invalid");
+        $("#length").addClass("valid");
+        $("#login-button").removeAttr('disabled');
+    }
 
+    if ($("#password").val() == $("#confirmPassword").val()) {
+        $("#match").removeClass("invalid");
+        $("#match").addClass("valid");
+        $("#register-button").removeAttr('disabled');
+    } else {
+        $("#match").removeClass("valid");
+        $("#match").addClass("invalid");
+        $("#register-button").prop("disabled", true);
+        
+    }
+});
 
-var basket = [];
+function passwordValidate() {
 
-function addProduct() {
-    basket.push("teste");
-    console.log(basket);
+    if ($("#password").val() == $("#confirmPassword").val()) {
+        $("#match").removeClass("invalid");
+        $("#match").addClass("valid");
+        $("#register-button").removeAttr('disabled');
+    } else {
+        $("#match").removeClass("valid");
+        $("#match").addClass("invalid");
+        $("#register-button").prop("disabled", true);
+    }
 }
 
-function test() {
-    console.log(basket);
-}
+
+$("#confirmPassword").focus(function () {
+    $("#confirmMessage").show();
+});
+
+// // When the user clicks outside of the password field, hide the message box
+
+$("#confirmPassword").blur(function () {
+    if ($("#password").val() == $("#confirmPassword").val()) {
+        $("#confirmMessage").hide();
+    } else {
+        $("#confirmMessage").show();
+    }
+});
+
+// When the user starts to type something inside the password field
+
+$("#confirmPassword").keyup(function () {
+    if ($("#password").val() == $("#confirmPassword").val()) {
+        $("#match").removeClass("invalid");
+        $("#match").addClass("valid");
+        $("#register-button").removeAttr('disabled');
+    } else {
+        $("#match").removeClass("valid");
+        $("#match").addClass("invalid");
+        $("#register-button").prop("disabled", true);
+    }
+});
